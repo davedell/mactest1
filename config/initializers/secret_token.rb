9ca5ed6708bce8969f285ccfa4dfa4ff87509405
Dave Dell <dave301@gmail.com>
2011-04-01 17:58:25 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mactest1::Application.config.secret_token = '4c6e901b6577ffd9c2ab387656c382299809a224c9d204a9dc2c816d14a4a4ef4bc78ff8937818ffae86a44a135bd3ee0361e2eb459ca4dd3f5e00bbc4b6ac6e'
